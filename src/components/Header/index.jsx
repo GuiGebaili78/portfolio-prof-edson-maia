68fe10b01b17c3610add9eb8deff0315ee8090e1
GuiGebaili78 <103890957+GuiGebaili78@users.noreply.github.com>
2025-03-24 21:14:41 -0300

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,18 @@
+import styles from './Header.module.css'
+
+
+function Header() {
+    return (
+        <header className={styles.header}>
+            <span>guilhermegebaili.dev</span>
+            <nav>
+                <a href="">Home</a>
+                <a href="">Sobre</a>
+                <a href="">Projetos</a>
+                <a href="">Contatos</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
