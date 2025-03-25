47e285d7f9d79e2855d758e8dd1e4c8f42a8f16a
GuiGebaili78 <103890957+GuiGebaili78@users.noreply.github.com>
2025-03-24 22:46:24 -0300

    Atualização da página home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import './App.css'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
       <Header />
+      <section className='container'>
+        <div className='apresentacao'>
+          <p>
+            Olá, sou <br />
+            <span>Guilherme Gebaili</span> <br />
+            Dev Full Stack
+          </p>
+          <button className='btn btn-red'>
+            Saiba mais sobre mim
+          </button>
+        </div>
+        <figure>
+          <img className='img-home'src="/img-in-the-office.svg" alt='Imagem de Home' />
+
+        </figure>
+
+      </section>
+      <Footer />
+
     </>
   )
 }
